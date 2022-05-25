64692f758d4e93870e462f9dc4acff9272c1e75a
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-05-25 15:41:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 #Python
 #importing the libraries
 import streamlit as st
-import os
-from gtts import gTTS
-from googletrans import Translator
-from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
-from  PIL import Image
-import jieba
-from xpinyin import Pinyin
+from gensim.summarization import summarize
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
 
 #main function
 def main():
@@ -17,60 +13,21 @@ def main():
     #select fairytale
     option = st.selectbox("How would you like to be contacted?", ("Little Snow White", "The Ugly Duckling", "Aladdin and the Wonderful Lamp", "The Sleeping Beauty", "Beauty and the Beast", "The Story of Cinderella", "Hansel and Gretel"))
     st.write('You selected:', option)
+    #if option == "Little Snow White":
+        #uploaded_file = st.file_uploader("")
+        #if uploaded_file:
+            #for line in uploaded_file:
+               # st.write(line)
+
     
     #give the user the option to choose between Text Summarization, Text processing and Translation
     activity1 = ["Summarize","Text Preprocessing", "Translation"]
     choice = st.sidebar.selectbox("Select Function",activity1)
-
-#translation function
-def translation_func(input_language, output_language, text):
-    translator = Translator()
-    translation = translator.translate(text, src=input_language, dest=output_language)
-    translation_text = translation.text
-    tts = gTTS(translation_text, lang=output_language,  slow=True)
-    try:
-        audio_file = text[0:20]
-    except:
-        audio_file = "audio"
-    tts.save(f"temp_folder/{audio_file}.mp3")
-    return audio_file, translation_text
-
-if choose == "Chinese":
-    if st.button("Show Translation and Audio"):
-        input='en'                   
-        output='zh-cn'
-        audio_file, translation_text = translation_func(input, output, text)
-
-        st.write('  ')
-        segments = jieba.cut(translation_text) #used for chinese text segmentation
-        seg_output = " ".join(segments)
-        html_str = f"""
-        <style>
-        p.a {{
-        font: bold {30}px Courier;
-        }}
-        </style>
-        <p class="a">{seg_output}</p>
-        """
-        st.markdown(html_str, unsafe_allow_html=True) #display the translated text in Chinese characters
-        st.write('  ')
-
-        p = Pinyin()
-        pinyined = p.get_pinyin(seg_output, splitter='', tone_marks='marks') #Get pinyin (the official romanization system for Standard Chinese in mainland China)
-        html_str2 = f"""
-        <style>
-        p.a {{
-        font: bold {25}px Courier;
-        }}
-        </style>
-        <p class="a">{pinyined}</p>
-        """
-        st.markdown(html_str2, unsafe_allow_html=True) #display pin yin
-
-        audio_file = open(f"temp_folder/{audio_file}.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format="audio/mp3", start_time=0) #displays the audio player 
-  
+    if choice == "Summarize":
+        st.subheader("Summary with NLP")
+        raw_text = st.text_area("Enter Text Here")
+        summary_choice = st.selectbox("Summary Choice", [Genism", "Sumy Lex Rank"])
+                                                       
 
  
 if __name__ == "__main__":
